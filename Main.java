b6b5bbe1cfc95a556808599fd1d89e914ac3c712
yroque <yadira_roque@hotmail.com>
2021-03-01 23:50:33 -0600

    Elimino una coma ps dev 4

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ class Main {
 
 	public static void main(String[] args){
 		System.out.println("Hola mundo, desde java");
-		System.out.println("La conexión con Github se realizado Tres  error")
-		System.out.println("tercer cambio");
+		System.out.println("La conexión con Github se realizado 4  error")
+		System.out.println("tercer cambio")
 		System.out.println("sexto cambio development error"
 	}
 
