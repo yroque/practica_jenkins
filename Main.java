878aaf2a2d96a72c3d02afa00494218d6bd2ef8d
yroque <yadira_roque@hotmail.com>
2021-03-01 23:19:00 -0600

    Cambio de mensaje sin coma

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
 
 	public static void main(String[] args){
 		System.out.println("Hola mundo, desde java");
-		System.out.println("La conexión con Github se realizado Tres");
+		System.out.println("La conexión con Github se realizado Tres")
 		System.out.println("tercer cambio");
 		System.out.println("sexto cambio");
 	}
