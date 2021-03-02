2d4b2f69c663200f7620b73e5e548ccaccc88913
yroque <yadira_roque@hotmail.com>
2021-03-01 20:45:17 -0600

    Cambio de mensaje DOS

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
 
 	public static void main(String[] args){
 		System.out.println("Hola mundo, desde java");
-		System.out.println("La conexión con Github se realizado");
+		System.out.println("La conexión con Github se realizado DOS");
 		System.out.println("tercer cambio");
 		System.out.println("sexto cambio");
 	}
