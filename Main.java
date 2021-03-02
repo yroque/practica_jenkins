fe0189a7a8c12b2fbe68b6cac01a28ba0a255bad
yroque <yadira_roque@hotmail.com>
2021-03-01 23:17:42 -0600

    fix cabeza separada

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
 
 	public static void main(String[] args){
 		System.out.println("Hola mundo, desde java");
-		System.out.println("La conexión con Github se ha hecho");
+		System.out.println("La conexión con Github se ha hecho")
 		System.out.println("tercer cambio");
 		System.out.println("sexto cambio");
 	}
